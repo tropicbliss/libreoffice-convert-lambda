cadd0497d3704e50c55e279be723c9b1b7578733
tropicbliss <tropicbliss@protonmail.com>
2025-06-30 23:03:09 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -128,7 +128,7 @@ function mainPage(content?: { type: "error" | "url"; data: string }) {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>File Download Test</title>
+        <title>Excel to PDF converter</title>
       </head>
 
       <body>
