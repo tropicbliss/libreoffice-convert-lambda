d5068ee554231d5044c6b21761ec7888af1c3d53
tropicbliss <tropicbliss@protonmail.com>
2025-07-04 15:58:10 +0800

    Remove delete s3 bucket script

--- scripts/delete-all-files.ts
+++
@@ -1,6 +0,0 @@
-import { execSync } from "child_process";
-import { Resource } from "sst";
-
-const command = `aws s3 rm s3://${Resource.Bucket.name} --recursive`;
-console.log(command);
-execSync(command);
